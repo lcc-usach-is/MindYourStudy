aa2c63394a0ccf9966e5b23bbd506e1fcf6b1606
Vicenthresh <Vicenthresh@users.noreply.github.com>
2021-02-04 02:59:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -120,26 +120,36 @@ def VentanaAbout():
     tabControl.add(tab2, text='Autor')
     tabControl.pack(expand=1, fill="both")
 
+    #### TAB INFORMACION ####
     frame = tk.Frame(tab1)
     frame.grid(row=0, column = 1, padx=10, pady=10, sticky="ew")
     ttk.Label(frame, text ="Mind your Study", font=("", 15, 'bold'),justify="center").grid(row = 0, column = 0,columnspan=2, sticky="w")
 
+    #version
     ttk.Label(tab1, text ="Version:").grid(column = 0,row = 1,padx = 5,pady = 10, sticky="e")
 
     #### TBD SACAR VERSION ACTUAL DESDE LA BASE DE DATOS CON UNA QUERY ####
     ttk.Label(tab1, text ="x.x.x").grid(column = 1,row = 1,padx = 0,pady = 10, sticky="w")
     #######################################################################
 
+    #pagina web
     ttk.Label(tab1, text ="Pagina web:").grid(column = 0,row = 2,padx = 5,pady = 10, sticky="e")
     link1 = tk.Label(tab1, text ="github.com/lcc-usach-is/MindYourStudy", fg = 'blue', cursor="hand2")
     link1.grid(column = 1,row = 2,padx = 0,pady = 10, sticky="w")
     link1.bind("<Button-1>", lambda e: callback("https://github.com/lcc-usach-is/MindYourStudy"))
 
+    #licencia
     ttk.Label(tab1, text ="Licencia:").grid(column = 0,row = 3,padx = 5,pady = 10, sticky="e")
     link2 = tk.Label(tab1, text ="GNU General Public License v3.0", fg = 'blue', cursor="hand2")
     link2.grid(column = 1,row = 3,padx = 0,pady = 10, sticky="w")
     link2.bind("<Button-1>", lambda e: callback("https://raw.githubusercontent.com/lcc-usach-is/MindYourStudy/main/LICENSE"))
 
+    #### TAB AUTOR ####
+
+    ttk.Label(tab2, text ="Autor 1:").grid(column=0, row=0 ,padx =20, pady = 30, sticky="e")
+    tk.Label(tab2, text ="autor1@algo.com").grid(column=1, row=0, padx=10, pady=10, sticky="w")
+
+    #boton de confirmacion para cerrar la ventana
     a = tk.Button(ventana, text="OK", command = ventana.destroy, relief = SOLID, bd=1, padx=10)
     a.place(x=330,y=210)
     buttons_ventana.append(a)
